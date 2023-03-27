9a2ef6d43838201bedc7f7709d1de30175155492
llcain <llcain2012@gmail.com>
2023-03-26 19:16:01 -0500

    added env file, models folder

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,32 @@
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000!!!')
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
